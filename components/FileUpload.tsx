d018fc1e70e136b2082752ce665bfef95be51103
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-05-14 10:52:41 +0800

    Implement drag and drop

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,27 +1,50 @@
-"use client";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { CloudArrowUpIcon } from "@heroicons/react/24/outline";
 import classNames from "classnames";
 const FileUpload = () => {
-  const ref = useRef<HTMLInputElement>(null);
+  const [fileList, setFileList] = useState<FileList | null>(null);
+  const [shouldHighlight, setShouldHighlight] = useState(false);
 
   return (
     <div
       className={classNames({
         "w-full h-96": true,
         "p-4 grid place-content-center cursor-pointer": true,
-        "bg-violet-50 text-violet-500 rounded-lg hover:bg-violet-100": true,
-        "border-4 border-dashed border-violet-100 hover:border-violet-500":
-          true,
+        "text-violet-500 rounded-lg": true,
+        "border-4 border-dashed ": true,
         "transition-colors": true,
+        "border-violet-500 bg-violet-100": shouldHighlight,
+        "border-violet-100 bg-violet-50": !shouldHighlight,
       })}
+      onDragOver={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setShouldHighlight(true);
+      }}
+      onDragLeave={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setShouldHighlight(false);
+      }}
+      onDragEnter={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setShouldHighlight(true);
+      }}
+      onDrop={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const files = e.dataTransfer.files;
+        console.log(files);
+        setFileList(files);
+        setShouldHighlight(false);
+      }}
     >
       <div className="flex flex-col items-center">
         <CloudArrowUpIcon className="w-10 h-10" />
         <span>
-          <span>Choose a File</span> or Drag it here
+          <span>Choose a File</span> or drag it here
         </span>
-        <input type="file" ref={ref} hidden />
       </div>
     </div>
   );
